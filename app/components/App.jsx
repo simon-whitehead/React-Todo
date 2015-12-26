8a245ade5596e775e738238e5a80c9ca31c3750b
Simon Whitehead <chemnova@gmail.com>
2015-12-26 23:28:18 +1100

    Slight formatting fixup

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,27 +2,33 @@ import uuid from 'node-uuid';
 import React from 'react';
 import NoteList from './NoteList.jsx';
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn Webpack'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do laundry'
-  }
-];
-
 export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <NoteList items={notes} />
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            notes: [
+                {
+                    id: uuid.v4(),
+                    task: 'Learn Webpack'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Learn React'
+                },
+                    {
+                    id: uuid.v4(),
+                    task: 'Do laundry'
+                }
+            ]
+        }
+    }
+
+    render() {
+        return (
+          <div>
+            <NoteList items={notes} />
+          </div>
+        );
+    }
 }
