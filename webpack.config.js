7359d8dc50511151485b383b79368694b8047714
Simon Whitehead <chemnova@gmail.com>
2015-12-28 16:34:59 +1100

    Converted build directory to 'content' and removed html generation from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,54 @@
 var path = require('path');
-var HtmlwebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
 
+var defaultPlugins = [
+    new webpack.HotModuleReplacementPlugin()
+];
+
 const PATHS = {
-  app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'content')
 };
 
+const PROD = JSON.parse(process.env.PROD || "0");
+
 module.exports = {
-  entry: PATHS.app,
-  output: {
-    path: PATHS.build,
-    filename: 'bundle.js'
-  },
-  resolve: {
-      extensions: ['', '.js', '.jsx']
-  },
-  module: {
-    loaders: [
-	{
-	    test: /\.css$/,
-	    loaders: ['style', 'css'],
-	    include: PATHS.app
-	},
-	{
-	    test: /\.jsx?$/,
-	    loaders: ['babel'],
-	    include: PATHS.app
-	}
-    ]
-  },
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    progress: true,
+    entry: PATHS.app,
+    output: {
+        path: PATHS.build,
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+        {
+            test: /\.css$/,
+            loaders: ['style', 'css'],
+            include: PATHS.app
+        },
+        {
+            test: /\.jsx?$/,
+            loaders: ['babel'],
+            include: PATHS.app
+        }
+        ]
+    },
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true,
 
-    // Display only errors to reduce the amount of output.
-    stats: 'errors-only',
+        // Display only errors to reduce the amount of output.
+        stats: 'errors-only',
 
-    // Parse host and port from env so this is easy to customize.
-    host: process.env.HOST,
-    port: process.env.PORT
-  },
-  plugins: [
-    new HtmlwebpackPlugin({
-      title: 'React Todo'
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ]
+        // Parse host and port from env so this is easy to customize.
+        host: process.env.HOST,
+        port: process.env.PORT
+    },
+    plugins: PROD ? defaultPlugins.concat([ 
+        new webpack.optimize.UglifyJsPlugin({ minimize: true }) 
+    ]) : defaultPlugins
 };
