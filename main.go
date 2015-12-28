124f4b7bbc7371590842cb86f1389d6334068534
Simon Whitehead <chemnova@gmail.com>
2015-12-29 00:41:33 +1100

    Added basic DB middleware wrapper

--- main.go
+++ main.go
@@ -1,8 +1,31 @@
 package main
 
-import "github.com/zenazn/goji"
+import (
+	"log"
+
+	"github.com/boltdb/bolt"
+	"github.com/zenazn/goji"
+
+	"github.com/simon-whitehead/react-todo/middleware"
+)
 
 func main() {
+	db := getBoltDb()
+	middleware.SetDatabase(db)
+
+	defer db.Close()
+
 	// flag.Set("bind", ":other_port_here") - the default is 8000
 	goji.Serve()
+}
+
+func getBoltDb() *bolt.DB {
+	db, err := bolt.Open("todo.db", 0600, nil)
+
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	return db
 }
---
+++ middleware/db.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/boltdb/bolt"
+	"github.com/zenazn/goji/web"
+)
+
+// db.go - Simple Goji middleware to pass
+// a boltdb instance into the context
+
+var (
+	db *bolt.DB
+)
+
+func SetDatabase(db *bolt.DB) {
+	db = db
+}
+
+func WithDB(c web.C, next http.Handler) http.Handler {
+	if db == nil {
+		panic(errors.New("Must set DB Middleware DB value"))
+	}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c.Env["Database"] = db
+	})
+}
